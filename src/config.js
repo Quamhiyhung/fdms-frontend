ca60cb132c5b6cad16af420f2a9a3c9b0780f53c
Quamhiyhung <derrickopokuagyei656@gmail.com>
2026-07-16 08:57:10 +0000

    Fix config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 const API_BASE =
-  process.env.REACT_APP_API_URL || https://fdms-backend-wfzz.onrender.com;
+  process.env.REACT_APP_API_URL || "https://fdms-backend-wfzz.onrender.com";
 
 export default API_BASE;
